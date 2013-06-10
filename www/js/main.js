9f1397fa26b9e77c848fff5ea6f76a9affb9305b
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-06-10 14:57:58 +0800

    test

--- www/js/main.js
+++ www/js/main.js
@@ -27,6 +27,17 @@ var main = function () {
                         if (typeof (device) != "undefined") {
                             registry.byId("txtPlatform").set("value", device.platform);
                             registry.byId("txtVersion").set("value", device.version);
+
+                            var states = {};
+                            states[Connection.UNKNOWN] = 'Unknown connection';
+                            states[Connection.ETHERNET] = 'Ethernet connection';
+                            states[Connection.WIFI] = 'WiFi connection';
+                            states[Connection.CELL_2G] = 'Cell 2G connection';
+                            states[Connection.CELL_3G] = 'Cell 3G connection';
+                            states[Connection.CELL_4G] = 'Cell 4G connection';
+                            states[Connection.NONE] = 'No network connection';
+
+                            registry.byId("txtConnection").set("value", states[navigator.network.connection.type]);
                         }
                     });
                 });
